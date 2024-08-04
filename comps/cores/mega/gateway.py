aebe1eea7312dbe6873c12a870b7861d9220648c
Chun Tao <chun.tao@intel.com>
2024-08-03 18:09:08 -0700

    test
    Signed-off-by: Chun Tao <chun.tao@intel.com>

--- comps/cores/mega/gateway.py
+++ comps/cores/mega/gateway.py
@@ -600,6 +600,6 @@ class AvatarChatbotGateway(Gateway):
 
         last_node = runtime_graph.all_leaves()[-1]
         # response = result_dict[last_node]["video_save_path"]
-        response = result_dict[last_node]
+        # response = result_dict
 
-        return response
+        return result_dict
