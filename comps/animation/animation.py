13e280fdea358880dcd3df17cbea9c278c3e14b3
Chun Tao <chun.tao@intel.com>
2024-08-04 20:01:49 -0700

    ffmpeg
    Signed-off-by: Chun Tao <chun.tao@intel.com>

--- comps/animation/animation.py
+++ comps/animation/animation.py
@@ -199,9 +199,14 @@ def animate(input: Base64ByteStrDoc):
     # )
     # subprocess.call(command, shell=platform.system() != "Windows")
 
-    ffmpeg.input(args.audio).input("temp/result.avi").output(
-        args.outfile, strict="-2", crf=23, vcodec="libx264", preset="medium", acodec="aac"
-    ).run()
+    ffmpeg.output(ffmpeg.input(args.audio),
+                  ffmpeg.input("temp/result.avi"),
+                  args.outfile, 
+                  strict='-2', 
+                  crf=23, 
+                  vcodec="libx264", 
+                  preset="medium", 
+                  acodec="aac").run()
 
     statistics_dict["opea_service@animation"].append_latency(time.time() - start, None)
     # return_str = f"Video generated successfully, check {args.outfile} for the result."
